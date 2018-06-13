b6c535821a3896e2a8fd9a340437fd8544b3e996
warren.kinser <warren.kinser@gmail.com>
2018-06-12 23:31:59 -0400

    Add functionality and fixed some bugs...

--- app.js
+++ app.js
@@ -7,17 +7,38 @@ const newShoppingListItemInput = document.querySelector('#shoppingListItem'),
     shoppingListElement = document.querySelector('#shoppingList');
 
     shoppingListElement.addEventListener('click', (event) => {
-        let target = event.target;
+      //console.log(event); 
+      let target = event.target;
 
         if (target.className === 'remove') {
             target.parentElement.remove();
         }
     });
 
-    addToShoppingListButton.addEventListener('click', (event) => {        
+//Event that add list item when we press enter
+    newShoppingListItemInput.addEventListener('keypress', function(event) {
+      if (event.key == "Enter") {
+        addToList();
+      }
+      console.log(event);
+    });
+    
+   addToShoppingListButton.addEventListener('click', function(event) {        
         let newShoppingListItemTemplate = `<li><input type='checkbox' class='complete'> <span>${newShoppingListItemInput.value}</span> <button class='remove'>remove</button></li>`;
                 
         shoppingListElement.innerHTML += newShoppingListItemTemplate;
     })
 
+
+function addToList() {
+  
+  if (newShoppingListItemInput.value.trim() == "") {
+      //nothing?
+  } else {
+  let newShoppingListItemTemplate = `<li><input type='checkbox' class='complete'> <span>${newShoppingListItemInput.value}</span> <button class='remove'>remove</button></li>`;
+               
+       shoppingListElement.innerHTML += newShoppingListItemTemplate;
+       newShoppingListItemInput.value = "";
+}
+}
 
